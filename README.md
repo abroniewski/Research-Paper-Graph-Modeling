4ffddde6a813b0fcff8c03fb9a6f0289bdeefc4e
Mohammad Ismail Tirmizi <ismailhero@gmail.com>
2022-03-06 23:00:10 +0100

    Code uploads file from neo4j scope

--- README.md
+++ README.md
@@ -18,12 +18,53 @@ LIMIT 3;
 ```
 
 ## Architecture/ Schema of the Graph
+Columns are in order so the ones to appear first, are first in file as well.
 
 ### Article
-- ID (Integer)
+- article (ID:Integer)
 - author (string[] - string array)
 - author-aux (string) 
+- Author-orcid 
+- booktitle:string
 - cite: (string[] )
-
+- cite-label: (string)
+- crossref: (string)
+- editor: (string[])
+- editor-orcid: (string[])
+- ee: (string[])
+- ee-type: (string[])
+- i: (string[])
+- journal: (string)
+- key:string
+- mdate:date
+- month:string
+- note:string[]
+- note-label:string
+- note-type:string[]
+- number:string
+- pages:string
+- publisher:string
+- publnr:string
+- publtype:string
+- sub:string[]
+- sup:string[]
+- title:string
+- title-bibtex:string
+- tt:string[]
+- url:string
+- volume:string
+- year:int
+- 
+### author_authored_by
+- article_id
+- author_id
 
 `string[]` - means string array.
+
+--relationships:published_by "output_psublisher_published_by.csv" 
+--relationships:edited_by "output_editor_edited_by.csv"  
+--relationships:published_in "output_journal_published_in.csv" 
+--relationships:has_citation "output_cite_has_citation.csv" 
+--relationships:authored_by "output_author_authored_by.csv"
+--relationships:submitted_at "output_school_submitted_at.csv" 
+--relationships:is_part_of "output_series_is_part_of.csv"
--- src/neo4j-db-initialize.py
+++ src/neo4j-db-initialize.py
@@ -1,3 +1,9 @@
+from neo4j import GraphDatabase
+import logging
+import sys
+from neo4j.exceptions import ServiceUnavailable
+import pandas as pd
+import csv
 # This python script is used to intialize the database and create all nodes and edges
 
 # link between python and Neo4j (Ismail)
@@ -29,3 +35,69 @@ CREATE(:Author {id: toInteger(line.id), name: line.author})
 
 
 ### Articles and Journals (Ismail)
+
+##################################################
+# Helper
+##################################################
+class Neo4jConnection:
+    def __init__(self, uri, user, pwd):
+        self.__uri = uri
+        self.__user = user
+        self.__pwd = pwd
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
+        except Exception as e:
+            print("Failed to create the driver:", e)
+
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+
+    def query(self, query, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try:
+            session = self.__driver.session(database=db) if db is not None else self.__driver.session()
+            response = list(session.run(query))
+        except Exception as e:
+            print("Query failed:", e)
+        finally:
+            if session is not None:
+                session.close()
+        return response
+
+
+# Python script for pre-processing checks
+
+main_file = "/home/teemo/.config/Neo4j Desktop/Application/relate-data/projects/project-ec569d0a-1cfc-4569-8aa8-cf7729f3de61/output/output_article.csv"
+
+with open("/home/teemo/.config/Neo4j Desktop/Application/relate-data/projects/project-ec569d0a-1cfc-4569-8aa8-cf7729f3de61/output/output_article_header.csv", 'r') as abfile:
+    header = csv.reader(abfile, delimiter=';')
+    header_list = next(header)
+    # print( header_list )
+    heading_clean = [ heading.split(':')[0] for heading in header_list ]
+    heading_str = ';'.join(heading_clean)
+    print(heading_str)
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+line_prepender(main_file, heading_str)
+
+conn = Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", pwd="lab1ml")
+query_string='''
+    LOAD CSV WITH HEADERS FROM 'http://localhost:11001/project-2aaa90a6-9ff2-437b-960f-e170f1a570de/output/output_article_2.csv' 
+        AS row FIELDTERMINATOR ';'
+    WITH row.article as article,
+    row.cite AS cite,
+    row["cite-label"] AS cite_label,
+    row["editor"] AS editor
+    return * LIMIT 30 ;
+    '''
+
+conn.query(query_string, db='neo4j')
