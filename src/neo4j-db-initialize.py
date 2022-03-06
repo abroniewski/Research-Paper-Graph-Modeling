487c9ca5b36698a225a29a0d0181dc782b5d555e
abroniewski <abroniewski@gmail.com>
2022-03-06 16:25:44 +0100

    initializing Python script for colab

--- src/neo4j-db-initialize.py
+++ src/neo4j-db-initialize.py
@@ -1,5 +1,14 @@
 # This python script is used to intialize the database and create all nodes and edges
 
+# link between python and Neo4j
+
 # Authors and Citations (Adam)
 
-# Articles and Journals (Ismail)
+# write Cypher query to create nodes and edges
+
+# Python script for pre-processing checks
+
+
+
+
+### Articles and Journals (Ismail)
