64c1ddb14936bf63470fd9340d4aab145dbf4f97
abroniewski <abroniewski@gmail.com>
2022-03-06 19:38:14 +0100

    Cypher for author nodes complete. Need to make python script to find citations to match selected authors (smaller file used).

--- src/neo4j-db-initialize.py
+++ src/neo4j-db-initialize.py
@@ -10,14 +10,17 @@ processed_data_directory = "data/processed/dblp-to-csv"
 ismail_directory = ""
 adam_directory ="http://localhost:11001/project-b7855507-1cee-4692-8ff3-f3e64ffe6e1a"
 #file location naming convention -> f"{name_directory}/output_school.csv"
-
+output_author.csv
 
 # Authors and Citations (Adam)
 
+# Creating nodes for authors
+LOAD CSV WITH HEADERS FROM 'http://localhost:11001/project-b7855507-1cee-4692-8ff3-f3e64ffe6e1a/output_author_small.csv' AS line FIELDTERMINATOR ';'
+CREATE(:Author {id: toInteger(line.id), name: line.author})
+
+#Creating citation csv for all authors that are currently in output_author_small
 
 
-LOAD CSV FROM f"{adam_directory}/output_school.csv" AS line
-CREATE (:Artist {name: line[1], year: toInteger(line[2])})
 # write Cypher query to create nodes and edges
 
 # Python script for pre-processing checks
